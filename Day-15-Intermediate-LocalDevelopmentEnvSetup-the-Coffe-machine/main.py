8a8a2713df93e96029d7df1ba7f24a3dc743daaa
mleiva2 <manuel.leiva@pmi.com>
2023-01-09 22:57:33 +0100

    fix and added while

--- Day-15-Intermediate-LocalDevelopmentEnvSetup-the-Coffe-machine/main.py
+++ Day-15-Intermediate-LocalDevelopmentEnvSetup-the-Coffe-machine/main.py
@@ -31,20 +31,50 @@ resources = {
 }
 
 def main():
-    user_input = input("What whould you like?")
-    if user_input == "off":
-        return print("Good bye")
-    elif user_input == "report":
-        print("Water:", resources["water"])
-        print("Milk:", resources["milk"])
-        print("Coffee:", resources["coffee"])
-        print("Money:") ## TODO: check the money 
-    elif user_input == "espresso":
-        print("hello")
-    elif user_input == "latte":
-        print("hello")
-    elif user_input == "cappuccino":
-        print("hello")
-
-
+    user_input = ""
+    while user_input != "off":
+        user_input = input("What whould you like?")
+        if user_input == "off":
+            return print("Good bye")
+        elif user_input == "report":
+            print("Water:", resources["water"])
+            print("Milk:", resources["milk"])
+            print("Coffee:", resources["coffee"])
+            print("Money:") ## TODO: check the money 
+        elif user_input == "coffee":
+            if resources["water"] >= MENU["espresso"]['ingredients']['water'] and resources["coffee"] >= MENU["espresso"]['ingredients']['coffee']:
+                print("Here is your espresso")
+            else:
+                if resources["water"] <= MENU["espresso"]['ingredients']['water']:
+                    print("Sorry, there's not enough water")
+                elif resources["coffee"] <= MENU["espresso"]['ingredients']['coffee']:
+                    print("Sorry, there's not enough coffee")
+        elif user_input == "latte":
+            if resources["water"] >= MENU["latte"]['ingredients']['water'] and resources["coffee"] >= MENU["latte"]['ingredients']['coffee'] and resources["milk"] >= MENU["latte"]['ingredients']['milk']:
+                print("Here is your latte")
+            else:
+                if resources["water"] <= MENU["latte"]['ingredients']['water']:
+                    print("Sorry, there's not enough water")
+                elif resources["coffee"] <= MENU["latte"]['ingredients']['coffee']:
+                    print("Sorry, there's not enough coffee")
+                elif resources["milk"] <= MENU["latte"]['ingredients']['milk']:
+                    print("Sorry, there's not enough milk")
+        elif user_input == "cappuccino":
+            if resources["water"] >= MENU["cappuccino"]['ingredients']['water'] and resources["coffee"] >= MENU["cappuccino"]['ingredients']['coffee'] and resources["milk"] >= MENU["cappuccino"]['ingredients']['milk']:
+                print("Here is your cappuccino")
+            else:
+                if resources["water"] <= MENU["cappuccino"]['ingredients']['water']:
+                    print("Sorry, there's not enough water")
+                elif resources["coffee"] <= MENU["cappuccino"]['ingredients']['coffee']:
+                    print("Sorry, there's not enough coffee")
+                elif resources["milk"] <= MENU["latte"]['ingredients']['milk']:
+                    print("Sorry, there's not enough milk")
 main()
+
+# def check_resources(user_input):
+#     if resources["water"] <= MENU["cappuccino"]['ingredients']['water']:
+#         print("Sorry, there's not enough water")
+#     elif resources["coffee"] <= MENU["cappuccino"]['ingredients']['coffee']:
+#         print("Sorry, there's not enough coffee")
+#     elif resources["milk"] <= MENU["latte"]['ingredients']['milk']:
+#         print("Sorry, there's not enough milk")
