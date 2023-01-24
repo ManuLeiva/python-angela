db484040f951f995e1974c874283d8dc6f6151a6
mleiva2 <manuel.leiva@pmi.com>
2023-01-24 15:29:44 +0100

    day16

---
+++ Day-16-Intermediate-OOO/main.py
@@ -0,0 +1,23 @@
+# import turtle
+
+# import another_module
+
+# print(another_module.another_variable)
+
+# timmy = turtle.Turtle()
+# timmy.shape("turtle")
+# timmy.color("red", "green")
+# timmy.forward(100)
+# my_screen = turtle.Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.field_names = ["Pokemon Name", "Type"]
+table.add_row(["Pikachu", "Electric"])
+table.add_row(["Charmander", "Fire"])
+table.add_row(["Squirtle", "Water"])
+table.align = "l"
+print(table)
